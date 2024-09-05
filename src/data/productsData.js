7b1b284427cd2387c1bd4b8103f97c87ca40faf6
Piyush-Kag <piyush.kag@innogent.in>
2024-09-05 11:45:33 +0530

    Added Files for project

---
+++ src/data/productsData.js
@@ -0,0 +1,68 @@
+const productsData = [
+    {
+        id: 1,
+        img: "/images/prod1.jpg",
+        rating: "★★★★★",
+        title: "Zebronix M123",
+        price: 1099,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "/images/prod2.jpg",
+        rating: "★★★★",
+        title: "Mobile Phone",
+        price: 649,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: "/images/prod3.jpg",
+        rating: "★★★★★",
+        title: "JBL Live 660NC",
+        price: 9999,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: "/images/prod4.jpg",
+        rating: "★★★★★",
+        title: "Pen Holder",
+        price: 99,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "/images/prod5.jpg",
+        rating: "★★★★",
+        title: "Black Belt",
+        price: 999,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "/images/prod6.jpg",
+        rating: "★★★★",
+        title: "DEX Drone TFS-82",
+        price: 5999,
+        quantity: 1,
+    },
+    {
+        id: 7,
+        img: "/images/prod7.jpg",
+        rating: "★★★★★",
+        title: "RGB RazerX Mouse",
+        price: 1599,
+        quantity: 1,
+    },
+    {
+        id: 8,
+        img: "/images/prod8.jpg",
+        rating: "★★★★",
+        title: "RGB Keyboard TLS7",
+        price: 3699,
+        quantity: 1,
+    }
+];
+
+export default productsData;
